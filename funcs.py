85c10c6d837793e91c312dcab46b865c7d7e7ef5
David <david@inaudit.io>
2023-10-25 15:33:13 +0200

    Fixed: validate user.

--- funcs.py
+++ funcs.py
@@ -7,6 +7,25 @@ import shutil
 from typing import List, Tuple, Any
 from fastapi import UploadFile
 
+import models as mod
+import asyncpg
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+EXTENSIONES_PERMITIDAS = {"jpg", "jpeg", "png"}
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Cargar variables de entorno
 load_dotenv() 
@@ -79,3 +98,77 @@ def procesar_imagen2(imagenes: List[Any], confianza: float, iou: float, cpu: int
 
     except Exception as e:
         raise Exception(f"Error al procesar las imágenes: {e}")
+
+async def get_database_connection():
+    conn = await asyncpg.connect(
+        user="postgres",
+        password="postgres",
+        database="guaitaia",
+        host="localhost",
+        port="5433"
+    )
+    return conn
+async def get_user_from_db(email: str):
+    conn = await get_database_connection()
+    try:
+        query = "SELECT * FROM users WHERE email = $1"
+        user_record = await conn.fetchrow(query, email)
+        if user_record:
+            return mod.User(**user_record)
+        return None
+    finally:
+        await conn.close()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+async def authenticate_user(email: str, password: str):
+    user = await get_user_from_db(email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = mod.TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
+    user = await get_user_from_db(email=token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(
+    current_user: Annotated[mod.User, Depends(get_current_user)]
+):
+    if current_user.is_active is False:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+def validar_extension(filename: str) -> bool:
+    try:
+        nombre, extension = filename.rsplit('.', 1)
+        return extension.lower() in EXTENSIONES_PERMITIDAS
+    except ValueError:
+        return False
--- main.py
+++ main.py
@@ -1,70 +1,26 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from typing import Annotated
-
-from fastapi import Depends, FastAPI, HTTPException, File, UploadFile, Form, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-from pydantic import BaseModel
-
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
-from datetime import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Depends, FastAPI, HTTPException, File, UploadFile, Form, status
 from PIL import Image
-from typing import List
 
 import funcs as fc 
+import models as mod
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/guaitaia"
 
-# generate password online
-# https://bcrypt.online/
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    },
-    "vigilant": {
-        "username": "vigilant",
-        "full_name": "John Doe",
-        "email": "vigilant@example.com",
-        "hashed_password": "$2y$10$VoPY4X96VpCTVYDCEjbgxu2FK9jjMqgGnKWD14bA7cgpVgaU.QeNO",
-        "disabled": False,
-    }
-}
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+Base = declarative_base()
 
 tags_metadata = [
     {
@@ -88,15 +44,6 @@ app = FastAPI(
     openapi_tags=tags_metadata
 )
 
-EXTENSIONES_PERMITIDAS = {"jpg", "jpeg", "png"}
-
-def validar_extension(filename: str) -> bool:
-    try:
-        nombre, extension = filename.rsplit('.', 1)
-        return extension.lower() in EXTENSIONES_PERMITIDAS
-    except ValueError:
-        return False
-
 # Configuración de CORS
 origins = [
     "*"
@@ -110,100 +57,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
+#@app.get("/")
+#async def root(
+#    current_user: Annotated[mod.User, Depends(fc.get_current_active_user)]
+#):
+#    user = await fc.get_user_from_db("admin@admin.com")
+#    return user
 
 @app.post(
     "/token", 
-    response_model=Token,
+    response_model=mod.Token,
     tags=["Authenticate"]
 )
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    user = await fc.authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+    access_token_expires = timedelta(minutes=fc.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = fc.create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
-
-"""
-@app.get(
-    "/users/me/", 
-    response_model=User,
-    tags=["Authenticate"]
-)
-async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return current_user
-"""
 
 app.mount("/imagenes", StaticFiles(directory="Resultados"), name="imagenes")
 
@@ -230,13 +110,13 @@ app.mount("/imagenes", StaticFiles(directory="Resultados"), name="imagenes")
     },
 )
 async def detectar_incendio(
-    current_user: Annotated[User, Depends(get_current_active_user)],
+    current_user: Annotated[mod.User, Depends(fc.get_current_active_user)],
     imagen: UploadFile = File(...),
     confianza: float = Form(...),
     iou: float = Form(...),
     cpu: int = Form(...)
 ):
-    if not validar_extension(imagen.filename):
+    if not fc.validar_extension(imagen.filename):
         raise HTTPException(status_code=400, detail="Formato de imagen no permitido")
 
     try:
@@ -245,7 +125,7 @@ async def detectar_incendio(
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al procesar la imagen: {e}")
 
-    ahora = datetime.now()
+    ahora = fc.datetime.now()
  
     return {
         "detecciones": detecciones,
@@ -258,14 +138,14 @@ async def detectar_incendio(
 
 @app.post("/detectar_incendios_multiples/", tags=["Multiple detection"])
 async def detectar_incendios_multiples(
-    current_user: Annotated[User, Depends(get_current_active_user)],
-    imagenes: List[UploadFile] = File(...),
+    current_user: Annotated[mod.User, Depends(fc.get_current_active_user)],
+    imagenes: fc.List[UploadFile] = File(...),
     confianza: float = Form(...),
     iou: float = Form(...),
     cpu: int = Form(...)
 ):
     for imagen in imagenes:
-        if not validar_extension(imagen.filename):
+        if not fc.validar_extension(imagen.filename):
             raise HTTPException(status_code=400, detail="Formato de imagen no permitido")
     
     try:
@@ -273,7 +153,7 @@ async def detectar_incendios_multiples(
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al procesar las imágenes: {e}")
 
-    ahora = datetime.now()
+    ahora = fc.datetime.now()
 
     return {
         "detecciones": detecciones,
