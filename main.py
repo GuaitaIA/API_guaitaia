988e15a54eacc44e9f19f2ab9a02d61523bd053c
David <david@inaudit.io>
2023-10-26 16:00:32 +0200

    Fixed: not receiving token if user is not active.

--- main.py
+++ main.py
@@ -89,6 +89,8 @@ async def login_for_access_token(
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
     access_token_expires = timedelta(weeks=fc.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = fc.create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
