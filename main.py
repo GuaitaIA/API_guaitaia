c62101717a3d53645c759abea0a2d29ba38675a8
davidsancho4 <davidsancho4@gmail.com>
2024-02-19 19:43:22 +0100

    Fixed: port 8000

--- main.py
+++ main.py
@@ -477,4 +477,4 @@ async def update_results_images_status(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
